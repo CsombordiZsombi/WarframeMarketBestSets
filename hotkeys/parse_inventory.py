96495cd9189915061d42aeddeec53a515f7cb869
CsombordiZsombi <zs.csomb@gmail.com>
2025-01-29 22:02:35 +0100

    geci

--- hotkeys/parse_inventory.py
+++ hotkeys/parse_inventory.py
@@ -7,6 +7,7 @@ from pynput.mouse import Controller
 import winsound
 import json
 from rapidfuzz.distance import Levenshtein
+import keyboard as kb
 
 SLOT_GRID = None
 SLOT_SIZE = None
@@ -17,15 +18,19 @@ BEEP = False
 mouse = Controller()
 
 def main(*args, **kvargs):
+    #if BEEP:
+    winsound.Beep(500, 300)
     start_time = time.time()
     print("Scanning inventory")
 
-    df = predict_inventory()
+    df, wdf = predict_inventory()
     df.to_json("inventory.json", orient="records", indent=4)
+    wdf.to_json("wrong_reads.json", orient="records", indent=4)
 
     print(f"Done scanning inventory, took {round(time.time()-start_time,1)} seconds")
 def predict_inventory():
     df = pd.DataFrame(columns=["item_name","quantity"])
+    wdf = pd.DataFrame(columns=["item_name","quantity"])
     
     with open("Warframe_UI/items.json") as file:
         global ITEM_LIST
@@ -48,31 +53,45 @@ def predict_inventory():
         
         for row in SLOT_GRID: # we predict and store the first 4 rows.
             for slot in row:
-                item = predict_item(slot, reader, sct)
-                if item["item_name"] == "":
-                    print("sor oszlop")
+                if kb.is_pressed('q'): 
                     return df
-                #if df["item_name"].str.contains(item["item_name"]).any():
-                #    return df
+                item = predict_item(slot, reader, sct)
+                if not item["item_name"][0]:
+                    print("üres string init")
+                    return df, wdf
+                if not item["item_name"][0] in ITEM_LIST:
+                    print(f'{item["item_name"][0]} nincs a listában init')
+                    wdf = pd.concat([wdf, pd.DataFrame(item)])
+                    continue
+                if df["item_name"].str.contains(item["item_name"][0]).any():
+                    print(f'{item["item_name"][0]} már felderítve init')
+                    return df, wdf
                 df = pd.concat([df, pd.DataFrame(item)])
-        
+        print("vege az init fázisnak 8==D")
         while True:
             mouse.scroll(0, -1) #scroll down
             time.sleep(0.5)
             for slot in SLOT_GRID[-1]:
+                if kb.is_pressed('q'): 
+                    return df, wdf
                 item = predict_item(slot, reader, sct)
-                if item["item_name"] == "":
-                    return df
-                if df["item_name"].str.contains(item["item_name"]).any():
-                    return df
+                if not item["item_name"][0]:
+                    print("üres string")
+                    return df, wdf
+                if not item["item_name"][0] in ITEM_LIST:
+                    print(f'{item["item_name"][0]} nincs a listában')
+                    wdf = pd.concat([wdf, pd.DataFrame(item)])
+                    continue
+                if df["item_name"].str.contains(item["item_name"][0]).any():
+                    print(f'{item["item_name"][0]} már felderítve')
+                    return df, wdf
                 df = pd.concat([df, pd.DataFrame(item)])
-    return df
+    return df, wdf
 def similar_item_name(word, max_distance=2):
-    global ITEM_LIST
     for w in ITEM_LIST:
         if Levenshtein.distance(word, w) <= max_distance:
             return w
-    return ""
+    return word
 
 def predict_item(slot, reader, sct):
     region = {
@@ -81,24 +100,25 @@ def predict_item(slot, reader, sct):
         "width":int(SLOT_SIZE[0]),
         "height":int(SLOT_SIZE[1])
         }
-    mouse.position = (int(slot[0]+SLOT_SIZE[0]/2), slot[1])
-    time.sleep(0.1)
+    mouse.position = (int(slot[0]+SLOT_SIZE[0]/2), slot[1]) # move the mouse
+    time.sleep(0.3)
     screenshot = sct.grab(region)
     img = np.array(screenshot)
     raw_text = reader.readtext(img, detail=0)
     item = process_raw_text(raw_text)
-    global ITEM_LIST
-    print(ITEM_LIST)
-    print(item)
-    if not (item["item_name"] in ITEM_LIST):
-        print("")
-        item['item_name'] = similar_item_name(item['item_name'])
+    print(f'\t item:{item}')
+
+    if not (item["item_name"][0] in ITEM_LIST):
+        item['item_name'] = [similar_item_name(item['item_name'])]
     if BEEP:
         winsound.Beep(500, 300)
     return item
                 
 def process_raw_text(raw_text):
     item = {"quantity":[1]}
+    if len(raw_text) == 0:
+        item["item_name"] = [""]
+        return item
     if len(raw_text) == 1: # if only item name in one line, and no quantity
         if "[" in raw_text[0]: # strip lvl
             item["item_name"] = [raw_text[0].split("[")[0].strip()]
@@ -122,13 +142,3 @@ def process_raw_text(raw_text):
     
     item["item_name"] = [item["item_name"].strip()]
     return item
-
-def test():
-    data = {"item_name":["asd","bsd"], "quantity":[1, 1]}
-    df = pd.DataFrame(data)
-    print(df["item_name"].str.contains("aasd").any())
-
-if __name__ == "__main__":
-    test()
-
-    
